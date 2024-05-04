c9e03c5a1ccada51a9b9a156bc4e1f6b1662f00a
franklynnaza <119573751+franklynnaza@users.noreply.github.com>
2024-05-04 09:27:24 +0000

    Add files via upload

---
+++ typped.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing",{
+    strings:["  Branding Services","Website Services"],
+    typeSpeed:30,
+    backSpeed:60,
+    loop:true
+})
+var typed = new Typed(".typingg",{
+    strings:["Why us ? We Dont just Build Websites,We Develop sales machine for your brand!" ],
+    typeSpeed:60,
+    backSpeed:10,
+    loop:false
+})
